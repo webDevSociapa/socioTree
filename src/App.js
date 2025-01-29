5ebf5cbf6790d4ddf73bf0e3456f41687b4956a0
webDevSociapa <webdev@sociapa.com>
2025-01-29 15:51:10 +0530

    fixesss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/common/header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeScreen from "./views/HomeScreen";
+// import AboutScreen from "../pages/AboutScreen";
+// import ContactScreen from "../pages/ContactScreen";
+import Header from "./components/common/header";
 
-function App() {
+const App = () => {
   return (
-    <>
-    <Header/>
-    </>
+    <Router>
+      <Header />  {/* Header will be consistent across all pages */}
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        {/* <Route path="/about" element={<AboutScreen />} />
+        <Route path="/contact" element={<ContactScreen />} /> */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
